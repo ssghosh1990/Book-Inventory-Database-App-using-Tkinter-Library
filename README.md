40f5af006fae1eeae1427e4a9a2371965b0a4145
ssghosh1990 <soumyashankar.ghosh@gmail.com>
2022-07-30 01:32:40 +0530

    Added update and close method in frontend script

---
+++ README.md
@@ -0,0 +1,2 @@
+This project includes building an book inventory db app using Tkinter library.
+SQLite is used as database server for this project.
--- frontend.py
+++ frontend.py
@@ -14,17 +14,20 @@ from tkinter import *
 import backend
 
 def get_selected_row(event):
-    global selected_tuple
-    index=list1.curselection()[0]
-    selected_tuple=list1.get(index)
-    e1.delete(0, END)
-    e1.insert(END,selected_tuple[1])
-    e2.delete(0, END)
-    e2.insert(END,selected_tuple[2])
-    e3.delete(0, END)
-    e3.insert(END,selected_tuple[3])
-    e4.delete(0, END)
-    e4.insert(END,selected_tuple[4])
+    try:
+        global selected_tuple
+        index=list1.curselection()[0]
+        selected_tuple=list1.get(index)
+        e1.delete(0,END)
+        e1.insert(END,selected_tuple[1])
+        e2.delete(0,END)
+        e2.insert(END,selected_tuple[2])
+        e3.delete(0,END)
+        e3.insert(END,selected_tuple[3])
+        e4.delete(0,END)
+        e4.insert(END,selected_tuple[4])
+    except IndexError:
+        pass
 
 def view_command():
     list1.delete(0,END)
@@ -44,10 +47,15 @@ def add_command():
 def delete_command():
     backend.delete(selected_tuple[0])
 
+def update_command():
+    backend.update(selected_tuple[0],title_text.get(), author_text.get(), year_text.get(), isbn_text.get())
+
 
 
 
 window=Tk()
+
+window.wm_title("BookStore")
 
 l1=Label(window, text="Title")
 l1.grid(row=0, column=0)
@@ -97,13 +105,13 @@ b2.grid(row=3, column=3)
 b3=Button(window, text="Add entry", width=12, command=add_command)
 b3.grid(row=4, column=3)
 
-b4=Button(window, text="Update", width=12)
+b4=Button(window, text="Update", width=12, command=update_command)
 b4.grid(row=5, column=3)
 
 b5=Button(window, text="Delete selected", width=12, command=delete_command)
 b5.grid(row=6, column=3)
 
-b6=Button(window, text="Close", width=12)
+b6=Button(window, text="Close", width=12,command=window.destroy )
 b6.grid(row=7, column=3)
 
 window.mainloop()
